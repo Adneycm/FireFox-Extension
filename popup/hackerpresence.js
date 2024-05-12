0663e88d1cc91eaabb05df22a914dc05ee9f2e36
Adneycm <adneycm@al.insper.edu.br>
2024-05-11 21:30:13 -0300

    fetch data from background.js

--- popup/hackerpresence.js
+++ popup/hackerpresence.js
@@ -1,18 +1,13 @@
-document.addEventListener("click", function (e) {
-  if (!e.target.classList.contains("hackerpresence-button")) {
-    return;
+document.addEventListener('DOMContentLoaded', function() {
+  browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
+      const currentTab = tabs[0];
+      browser.runtime.sendMessage({request: "getData", tabId: currentTab.id}).then(updatePopup);
+  });
+
+  function updatePopup(response) {
+      document.getElementById('third_party').textContent = `Third Party Requests: ${response.thirdPartyRequests}`;
+      document.getElementById('cookies').textContent = `Cookies: ${response.cookiesCount}`;
+      document.getElementById('local_storage').textContent = `Local Storage Items: ${response.localStorageItems}`;
+      document.getElementById('hijacking').textContent = `Session Storage Items: ${response.sessionStorageItems}`;
   }
-  console.log("pass return function listener!");
-
-
-  var hacker = e.target.textContent;
-  console.log(hacker);
-
-  chrome.tabs.executeScript(null, {
-    file: "/content_scripts/hackerpresencefy.js",
-  });
-
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { hackerCheck: hacker });
-  });
 });
